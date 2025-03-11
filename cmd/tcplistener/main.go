e700b0976a993bb5f57b4e4f6b1c390e851b2928
per1Peteia <justusmanthey@googlemail.com>
2025-03-11 13:38:31 +0100

    tcplistener uses RequestFromRead to read and parse curl connections

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,56 +1,54 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
+	"github.com/per1Peteia/httpfromtcp/internal/request"
 	"log"
 	"net"
-	"strings"
 )
 
 const port string = ":42069"
 
 // the commented-out code is left here to visualize the developement of the course-work
 
-func getLinesChannel(conn net.Conn) <-chan string {
-	var lines = make(chan string)
-
-	go func() {
-		readBuf := make([]byte, 8)
-		var lineBuf bytes.Buffer
-
-		for {
-			n, err := conn.Read(readBuf)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					if lineBuf.Len() > 0 {
-						lines <- lineBuf.String()
-					}
-					close(lines)
-					fmt.Printf("connection (%s) has been closed\n", conn.RemoteAddr().String())
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				break
-			}
-
-			str := string(readBuf[:n])
-			if strings.Contains(str, "\n") {
-				i := strings.Index(str, "\n")
-				lineBuf.WriteString(str[:i])
-				lines <- lineBuf.String()
-
-				lineBuf.Reset()
-				lineBuf.WriteString(str[i+1:])
-				continue
-			}
-			lineBuf.WriteString(str)
-		}
-	}()
-	return lines
-}
+// refactor: connecting http request line parser to tcplistener
+// func getLinesChannel(conn net.Conn) <-chan string {
+// 	var lines = make(chan string)
+//
+// 	go func() {
+// 		readBuf := make([]byte, 8)
+// 		var lineBuf bytes.Buffer
+//
+// 		for {
+// 			n, err := conn.Read(readBuf)
+// 			if err != nil {
+// 				if errors.Is(err, io.EOF) {
+// 					if lineBuf.Len() > 0 {
+// 						lines <- lineBuf.String()
+// 					}
+// 					close(lines)
+// 					fmt.Printf("connection (%s) has been closed\n", conn.RemoteAddr().String())
+// 					break
+// 				}
+// 				fmt.Printf("error: %s\n", err.Error())
+// 				break
+// 			}
+//
+// 			str := string(readBuf[:n])
+// 			if strings.Contains(str, "\n") {
+// 				i := strings.Index(str, "\n")
+// 				lineBuf.WriteString(str[:i])
+// 				lines <- lineBuf.String()
+//
+// 				lineBuf.Reset()
+// 				lineBuf.WriteString(str[i+1:])
+// 				continue
+// 			}
+// 			lineBuf.WriteString(str)
+// 		}
+// 	}()
+// 	return lines
+// }
 
 func main() {
 	// refactor: file-reading -> tcp-connection
@@ -75,9 +73,14 @@ func main() {
 		}
 		fmt.Printf("connection accepted from: %s\n", conn.RemoteAddr().String())
 
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("%s\n", line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Printf(
+			"Request line:\n- Method: %s\n- Target: %s\n- Version: %s",
+			req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion,
+		)
 	}
 
 }
