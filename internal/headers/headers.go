a2bec6473a8ae8d4071f16742f6c6b964b491683
per1Peteia <justusmanthey@googlemail.com>
2025-03-16 21:11:04 +0100

    multiple values for single header + tests

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 type Headers map[string]string
@@ -23,17 +24,46 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
 	// parse the data
 	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
-	key := string(parts[0])
+	key := strings.ToLower(string(parts[0]))
 	if key != strings.TrimRight(key, " ") {
-		return 0, false, fmt.Errorf("error: invalid field-name found (%s)", key)
+		return 0, false, fmt.Errorf("error: invalid field-name found (%s) - whitespace", key)
+	}
+	key = strings.TrimSpace(key)
+	if !validFieldName(key) {
+		return 0, false, fmt.Errorf("error: invalid field-name (%s) - illegal codepoint", key)
 	}
 	value := bytes.TrimSpace(parts[1])
-	key = strings.TrimSpace(key)
+
 	h.Set(key, string(value))
 
 	return idx + 2, false, nil
 }
 
 func (h Headers) Set(key, value string) {
+	if _, exists := h[key]; exists {
+		h[key] += ", " + value
+		return
+	}
 	h[key] = value
+}
+
+// range table to use as valid codepoints for field-names
+var validRunes = &unicode.RangeTable{
+	R16: []unicode.Range16{
+		{
+			Lo:     0x0021,
+			Hi:     0x007E,
+			Stride: 1,
+		},
+	},
+}
+
+// takes a string as input and checks whether it is within the range of valid, specified unicode codepoints
+func validFieldName(str string) bool {
+	for _, char := range str {
+		if !unicode.In(char, validRunes) {
+			return false
+		}
+	}
+	return true
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -17,7 +17,7 @@ func TestParseHeaders(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -35,7 +35,7 @@ func TestParseHeaders(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 29, n)
 	assert.False(t, done)
 
@@ -58,6 +58,42 @@ func TestParseHeaders(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, headers)
 	assert.Equal(t, "test11", headers["test1"])
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.False(t, done)
+
+	// Test: invalid character in field name
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: field value is guaranteed to be lower case
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+
+	// Test: Multiple field values set to same field name
+	headers = NewHeaders()
+	data = []byte("Set-Person: lane-loves-go\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.False(t, done)
+	data = []byte("Set-Person: prime-loves-zig\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.False(t, done)
+	data = []byte("Set-Person: tj-loves-ocaml\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.False(t, done)
+
+	assert.Equal(t, "lane-loves-go, prime-loves-zig, tj-loves-ocaml", headers["set-person"])
 }
